68974214d11e1d5c66831026f961a90eb9fcbb68
Miguel Salvador <miguel_f_r_salvador@hotmail.com>
2024-06-24 06:35:58 +0100

    ex

---
+++ 6kyu/Dashatize it.js
@@ -0,0 +1,13 @@
+// Given an integer, return a string with dash '-' marks before and after each odd digit, but do not begin or end the string with a dash mark.
+
+// Ex:
+
+// 274 -> '2-7-4'
+// 6815 -> '68-1-5'
+
+function dashatize(num) {
+    return num = num.toString().split("").map(function(c){
+      return c % 2 ? "-" + c + "-" :  c ;
+    }).join("").split("-").filter(a => a != "").join("-");
+    
+  };
